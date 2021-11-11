70b763d801cb32d18bc3ff71a97c18efff18c68b
Bionca Bond <bioncab@zillowgroup.com>
2021-11-10 21:20:17 -0800

    setup

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-bootstrap';
+//import { withAuth0 } from '@auth0/auth0-react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>This is the Header</h1>
+            </>
+        )
+    }
+}
+
+export default Header;
