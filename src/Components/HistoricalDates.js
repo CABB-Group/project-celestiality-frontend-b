be70b2a72947ef74f34d25c84b173f102721287b
Chris Reichert <chris.a.reichert@gmail.com>
2021-11-17 23:36:12 -0600

    styled the accordian

--- src/Components/HistoricalDates.js
+++ src/Components/HistoricalDates.js
@@ -4,13 +4,14 @@ import { Accordion } from 'react-bootstrap';
 
 class HistoricalDates extends React.Component {
   render() {
+    // console.log('this is historicalDate:' ,this.props);
     return (
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Your Birthday Historical Facts!</Accordion.Header>
+      <Accordion defaultActiveKey="0" style={{marginTop:'15px'}}>
+        <Accordion.Item eventKey="0" style={{minWidth:'30rem', margin:'0px auto'}}>
+          <Accordion.Header>On your Birthdate:</Accordion.Header>
             {this.props.historydates.map((item, idx) => (
-          <Accordion.Body key={idx}>
-              <p > {item.description} </p>
+          <Accordion.Body key={idx} style={{textAlign:'left', margin:'0px',padding:'0px'}}>
+              <p style={{maxHeight:'.5rem', margin:'0px',fontSize:'.7rem'}}>In the year {item.year}: {item.description} </p>
               </Accordion.Body>
             ))}
         </Accordion.Item>
